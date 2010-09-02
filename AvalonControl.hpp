68b62100895d0dca1ba56f242244c353868fa66d
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2010-09-02 17:28:47 +0200

    fixed thruster dir

--- AvalonControl.hpp
+++ AvalonControl.hpp
@@ -42,10 +42,10 @@ namespace avalon_control
     {
         DIR_MIDDLE_HORIZONTAL = -1,
         DIR_MIDDLE_VERTICAL   = 1,
-        DIR_REAR_HORIZONTAL   = 1,
+        DIR_REAR_HORIZONTAL   = -1,
         DIR_REAR_VERTICAL     = -1,
         DIR_RIGHT = -1,
-        DIR_LEFT  = -1
+        DIR_LEFT  = 1
     };
 
     struct MotionControllerState
