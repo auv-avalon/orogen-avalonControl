6a84768077aee849b85626c0572dead46343cf4c
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2011-05-10 08:57:17 +0200

    Thruster order

--- AvalonControl.hpp
+++ AvalonControl.hpp
@@ -18,9 +18,9 @@ namespace avalon_control
 
     enum MOTCON_CHANNEL_DIRECTIONS
     {
-        DIR_MIDDLE_HORIZONTAL = 1,
+        DIR_MIDDLE_HORIZONTAL = -1,
         DIR_MIDDLE_VERTICAL   = 1,
-        DIR_REAR_HORIZONTAL   = -1,
+        DIR_REAR_HORIZONTAL   = 1,
         DIR_REAR_VERTICAL     = 1,
         DIR_RIGHT = -1,
         DIR_LEFT  = -1
