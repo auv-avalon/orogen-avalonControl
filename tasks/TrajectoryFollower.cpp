362b234acf38293652d1376023a10645f90fc8fa
Matthias Goldhoorn <Matthias@Goldhoorn.eu>
2014-04-14 22:16:31 +0200

    added paramters for easier debugging

--- tasks/TrajectoryFollower.cpp
+++ tasks/TrajectoryFollower.cpp
@@ -7,11 +7,13 @@ using namespace avalon_control;
 TrajectoryFollower::TrajectoryFollower(std::string const& name, TaskCore::TaskState initial_state)
     : TrajectoryFollowerBase(name, initial_state)
 {
+    _final_heading.set(base::unset<double>());
 }
 
 TrajectoryFollower::TrajectoryFollower(std::string const& name, RTT::ExecutionEngine* engine, TaskCore::TaskState initial_state)
     : TrajectoryFollowerBase(name, engine, initial_state)
 {
+    _final_heading.set(base::unset<double>());
 }
 
 TrajectoryFollower::~TrajectoryFollower()
@@ -52,6 +54,13 @@ void TrajectoryFollower::updateHook()
     std::pair<Eigen::Vector3d ,Eigen::Vector3d> p2 = spline.getPointAndTangent(p.first);
     Eigen::Vector3d next_point = p2.first;
     printf("first: %f, end: %f\n",p.first,spline.getEndParam());
+    base::AUVPositionCommand cmd;
+    cmd.x = next_point[0];
+    cmd.y = next_point[1];
+    cmd.z = next_point[2];
+    Eigen::Vector3d direction = p2.second;
+    direction.normalize();
+    double heading = acos(direction[0]);
     if(fabs(p.first - spline.getEndParam()) < 0.001){
         if(!endReached){
             endReachTime = rbs.time;
@@ -61,15 +70,14 @@ void TrajectoryFollower::updateHook()
         if((rbs.time - endReachTime).toSeconds() >  _timeout_at_end_before_mark_end_reached.get()){
             state(REACHED_END);
         }
+        if(!base::isUnset<double>(_final_heading.get())){
+            heading = _final_heading.get();
+        }
     }
-    base::AUVPositionCommand cmd;
-    cmd.x = next_point[0];
-    cmd.y = next_point[1];
-    cmd.z = next_point[2];
-    Eigen::Vector3d direction = p2.second;
-    direction.normalize();
-    double heading = acos(direction[0]);
     if(direction[1] < 0) heading*=-1;
+    if(_use_zero_heading.get()){
+        heading = 0;
+    }
     cmd.heading = heading;
     _position_command.write(cmd);
     base::Waypoint wp(next_point,heading,0,0);
